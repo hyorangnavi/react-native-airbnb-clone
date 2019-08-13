997ad727b2bdd9391bd0715a31a088975ecbb17b
uPrin <503109@naver.com>
2019-08-14 03:36:36 +0900

    Finish Project
    Finish Project

--- screens/Explore.js
+++ screens/Explore.js
@@ -9,28 +9,56 @@ import {
   StatusBar,
   ScrollView,
   Image,
-  Dimensions
+  Dimensions,
+  Animated
 } from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
 import Category from "./components/Explore/Category";
 import Home from "./components/Explore/Home.js";
+import Tag from "./components/Explore/Tag.js";
 
 const { height, width } = Dimensions.get("window");
 class Explore extends Component {
   componentWillMount() {
+    this.scrollY = new Animated.Value(0);
     this.startHeaderHeight = 80;
+    this.endHeaderHeight = 50;
     if (Platform.OS == "android") {
       this.startHeaderHeight = 100 + StatusBar.currentHeight;
+      this.endHeaderHeight = 70 + StatusBar.currentHeight;
     }
+
+    this.animatedHeaderHeight = this.scrollY.interpolate({
+      inputRange: [0, 50],
+      outputRange: [this.startHeaderHeight, this.endHeaderHeight],
+      extrapolate: "clamp"
+    });
+    this.animatedOpacity = this.animatedHeaderHeight.interpolate({
+      inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+      outputRange: [0, 1],
+      extrapolate: "clamp"
+    });
+
+    this.animatedTagTop = this.animatedHeaderHeight.interpolate({
+      inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+      outputRange: [-30, 10],
+      extrapolate: "clamp"
+    });
+
+    this.animatedMarginTop = this.animatedHeaderHeight.interpolate({
+      inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+      outputRange: [50, 30],
+      extrapolate: "clamp"
+    });
   }
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
-          <View
+          <Animated.View
             style={{
-              height: this.startHeaderHeight,
+              height: this.animatedHeaderHeight,
               backgroundColor: "white",
               borderBottomWidth: 1,
               borderBottomColor: "#dddddd"
@@ -61,11 +89,26 @@ class Explore extends Component {
                 }}
               />
             </View>
-          </View>
-          <ScrollView scrollEventThrottle={16}>
-            <View
-              style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}
+            <Animated.View
+              style={{
+                flexDirection: "row",
+                marginHorizontal: 20,
+                position: "relative",
+                top: this.animatedTagTop,
+                opacity: this.animatedOpacity
+              }}
             >
+              <Tag name="Guests" />
+              <Tag name="Dates" />
+            </Animated.View>
+          </Animated.View>
+          <ScrollView
+            scrollEventThrottle={16}
+            onScroll={Animated.event([
+              { nativeEvent: { contentOffset: { y: this.scrollY } } }
+            ])}
+          >
+            <View style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}>
               <Text
                 style={{
                   fontSize: 24,
@@ -101,9 +144,7 @@ class Explore extends Component {
                 <Text style={{ fontWeight: "100", marginTop: 10 }}>
                   A new selection of homes verified for quality & comfort
                 </Text>
-                <View
-                  style={{ width: width - 40, height: 200, marginTop: 20 }}
-                >
+                <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
                   <Image
                     style={{
                       flex: 1,
