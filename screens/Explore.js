1e02553bd097bed5e1d53b9a9988a29ee070e9f0
uPrin <503109@naver.com>
2019-08-14 02:44:51 +0900

    Added Home Func
    Added Home Func

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,88 +1,185 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput,
-    Platform,
-    StatusBar,
-    ScrollView,
-    Image,
-    Dimensions
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar,
+  ScrollView,
+  Image,
+  Dimensions
 } from "react-native";
 
-import Icon from 'react-native-vector-icons/Ionicons'
-import Category from './components/Explore/Category'
+import Icon from "react-native-vector-icons/Ionicons";
+import Category from "./components/Explore/Category";
+import Home from "./components/Explore/Home.js";
 
-const {height,width} = Dimensions.get('window')
+const { height, width } = Dimensions.get("window");
 class Explore extends Component {
-
-    componentWillMount() {
-        this.startHeaderHeight = 80
-        if (Platform.OS == 'android') {
-        this.startHeaderHeight = 100 + StatusBar.currentHeight
-                }
+  componentWillMount() {
+    this.startHeaderHeight = 80;
+    if (Platform.OS == "android") {
+      this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
-    render() {
-        return (
-            <SafeAreaView style={{flex:1}}>
-                <View style={{flex:1}}>
-                    <View style={{height:this.startHeaderHeight, backgroundColor:'white', borderBottomWidth:1, borderBottomColor:'#dddddd'}}>
-                        <View style={{flexDirection:'row',
-                                        padding:10,
-                                        backgroundColor:'white',
-                                        marginHorizontal:20,
-                                        shadowOffset:{width:0,height:0},
-                                        shadowColor:'black',
-                                        shadowOpacity: 0.2,
-                                        elevation:1,
-                                        marginTop:Platform.OS=='android' ? 30 : null}}>
-                            <Icon name="ios-search" size={20}/>
-                            <TextInput
-                                underlineColorAndroid="transparent"
-                                placeholder="Try New Seoul"
-                                placeholderTextColor="grey"
-                                style={{flex:1, fontWeight:'700', backgroundColor:'white'}}/>
-                        </View>
-                    </View>
-                    <ScrollView
-                    scrollEventThrottle={16}>
-                        <View style={{flex:1, backgroundColor:'white',paddingTop:20}}>
-                            <Text style={{fontSize:24,fontWeight:'700',paddingHorizontal:20}}>
-                                What can we help you fine, Hyowon?
-                            </Text>
-                            <View style={{height:130,marginTop:20}}>
-                                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                                    <Category imageUri={require('../assets/home.jpg')} name="Home"/>
-                                    <Category imageUri={require('../assets/experiences.jpg')} name="Experiences"/>
-                                    <Category imageUri={require('../assets/restaurant.jpg')} name="Restaurant"/>
-                                </ScrollView>
-                            </View>
-                            <View style={{marginTop:40, paddingHorizontal:20}}>
-                                <Text style={{fontSize:24,fontWeight:'700'}}>
-                                    Introducing Airbnb Plus
-                                </Text>
-                                <Text style={{fontWeight:'100',marginTop:10}}>
-                                    A new selection of homes verified for quality & comfort
-                                </Text>
-                                <View style={{width:width-40,height:200, marginTop:20}}>
-                                    <Image style={{flex:1,height:null,width:null,resizeMode:'cover',borderRadius:5,borderWidth:1,borderColor:'#dddddd'}} source={require('../assets/home.jpg')}/>
-                                </View>
-                            </View>
-                        </View> 
-                    </ScrollView>
+  }
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              height: this.startHeaderHeight,
+              backgroundColor: "white",
+              borderBottomWidth: 1,
+              borderBottomColor: "#dddddd"
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                padding: 10,
+                backgroundColor: "white",
+                marginHorizontal: 20,
+                shadowOffset: { width: 0, height: 0 },
+                shadowColor: "black",
+                shadowOpacity: 0.2,
+                elevation: 1,
+                marginTop: Platform.OS == "android" ? 30 : null
+              }}
+            >
+              <Icon name="ios-search" size={20} />
+              <TextInput
+                underlineColorAndroid="transparent"
+                placeholder="Try New Seoul"
+                placeholderTextColor="grey"
+                style={{
+                  flex: 1,
+                  fontWeight: "700",
+                  backgroundColor: "white"
+                }}
+              />
+            </View>
+          </View>
+          <ScrollView scrollEventThrottle={16}>
+            <View
+              style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}
+            >
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "700",
+                  paddingHorizontal: 20
+                }}
+              >
+                What can we help you fine, Hyowon?
+              </Text>
+              <View style={{ height: 130, marginTop: 20 }}>
+                <ScrollView
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                >
+                  <Category
+                    imageUri={require("../assets/home.jpg")}
+                    name="Home"
+                  />
+                  <Category
+                    imageUri={require("../assets/experiences.jpg")}
+                    name="Experiences"
+                  />
+                  <Category
+                    imageUri={require("../assets/restaurant.jpg")}
+                    name="Restaurant"
+                  />
+                </ScrollView>
+              </View>
+              <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                <Text style={{ fontSize: 24, fontWeight: "700" }}>
+                  Introducing Airbnb Plus
+                </Text>
+                <Text style={{ fontWeight: "100", marginTop: 10 }}>
+                  A new selection of homes verified for quality & comfort
+                </Text>
+                <View
+                  style={{ width: width - 40, height: 200, marginTop: 20 }}
+                >
+                  <Image
+                    style={{
+                      flex: 1,
+                      height: null,
+                      width: null,
+                      resizeMode: "cover",
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: "#dddddd"
+                    }}
+                    source={require("../assets/home.jpg")}
+                  />
                 </View>
-            </SafeAreaView>
-        );
-    }
+              </View>
+            </View>
+            <View style={{ marginTop: 40 }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "700",
+                  paddingHorizontal: 20
+                }}
+              >
+                Homes around the world
+              </Text>
+              <View
+                style={{
+                  paddingHorizontal: 20,
+                  marginTop: 20,
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  justifyContent: "space-between"
+                }}
+              >
+                <Home
+                  width={width}
+                  name="The Cozy Place"
+                  type="PRIVATE ROOM - 2 BEDS"
+                  price={82}
+                  rating={4}
+                />
+                <Home
+                  width={width}
+                  name="Lotte Tower Residence"
+                  type="SUPER SIZE"
+                  price={682}
+                  rating={4.5}
+                />
+                <Home
+                  width={width}
+                  name="Tokio Hotel"
+                  type="PRIVATE ROOM - 3 BEDS"
+                  price={221}
+                  rating={5}
+                />
+                <Home
+                  width={width}
+                  name="The Cozy Place"
+                  type="PRIVATE ROOM - 1 BEDS"
+                  price={42}
+                  rating={4}
+                />
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 export default Explore;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
