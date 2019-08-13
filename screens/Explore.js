2c3648b8e5a95c3eb15e9f79794c39b1f741f96c
uPrin <503109@naver.com>
2019-08-14 01:59:41 +0900

    Added ScrollViews

--- screens/Explore.js
+++ screens/Explore.js
@@ -6,11 +6,16 @@ import {
     SafeAreaView,
     TextInput,
     Platform,
-    StatusBar
+    StatusBar,
+    ScrollView,
+    Image,
+    Dimensions
 } from "react-native";
 
 import Icon from 'react-native-vector-icons/Ionicons'
+import Category from './components/Explore/Category'
 
+const {height,width} = Dimensions.get('window')
 class Explore extends Component {
 
     componentWillMount() {
@@ -38,10 +43,35 @@ class Explore extends Component {
                                 underlineColorAndroid="transparent"
                                 placeholder="Try New Seoul"
                                 placeholderTextColor="grey"
-                                style={{flex:1, fontWeight:'700', backgroundColor:'white'}}
-                            />
+                                style={{flex:1, fontWeight:'700', backgroundColor:'white'}}/>
                         </View>
                     </View>
+                    <ScrollView
+                    scrollEventThrottle={16}>
+                        <View style={{flex:1, backgroundColor:'white',paddingTop:20}}>
+                            <Text style={{fontSize:24,fontWeight:'700',paddingHorizontal:20}}>
+                                What can we help you fine, Hyowon?
+                            </Text>
+                            <View style={{height:130,marginTop:20}}>
+                                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                                    <Category imageUri={require('../assets/home.jpg')} name="Home"/>
+                                    <Category imageUri={require('../assets/experiences.jpg')} name="Experiences"/>
+                                    <Category imageUri={require('../assets/restaurant.jpg')} name="Restaurant"/>
+                                </ScrollView>
+                            </View>
+                            <View style={{marginTop:40, paddingHorizontal:20}}>
+                                <Text style={{fontSize:24,fontWeight:'700'}}>
+                                    Introducing Airbnb Plus
+                                </Text>
+                                <Text style={{fontWeight:'100',marginTop:10}}>
+                                    A new selection of homes verified for quality & comfort
+                                </Text>
+                                <View style={{width:width-40,height:200, marginTop:20}}>
+                                    <Image style={{flex:1,height:null,width:null,resizeMode:'cover',borderRadius:5,borderWidth:1,borderColor:'#dddddd'}} source={require('../assets/home.jpg')}/>
+                                </View>
+                            </View>
+                        </View> 
+                    </ScrollView>
                 </View>
             </SafeAreaView>
         );
